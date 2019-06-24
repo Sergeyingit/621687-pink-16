00d01339c3884392f3fd7fc61d31492f3d291452
sergeyingit <sergeyakkaynt@gmail.com>
2019-06-24 03:53:05 +0300

    добавляет гугл карту, мелкие правки по ПП, добавляет полифил и заменяет тег main на див для ие11

--- source/js/js.js
+++ source/js/js.js
@@ -5,7 +5,16 @@ var toggleNavigation = document.querySelector('.main-navigation__toggle');
 pageHeader.classList.remove('page-header--nojs');
 mainNavigation.classList.remove('main-navigation--nojs');
 
+
 toggleNavigation.addEventListener('click', function () {
+  if (pageHeader.classList.contains('page-header--closed')) {
+    pageHeader.classList.remove('page-header--closed');
+    pageHeader.classList.add('page-header--opened');
+  } else {
+    pageHeader.classList.add('page-header--closed');
+    pageHeader.classList.remove('page-header--opened');
+  }
+
   if (mainNavigation.classList.contains('main-navigation--closed')) {
     mainNavigation.classList.remove('main-navigation--closed');
     mainNavigation.classList.add('main-navigation--opened');
